997c194acffe2c6fda4e3a4c2ae29b58b919dd03
making <makingx@gmail.com>
2017-02-04 20:13:07 +0900

    Enable OAuth2 Authorization

--- src/main/java/jsug/flaky/members/FlakyMembersApplication.java
+++ src/main/java/jsug/flaky/members/FlakyMembersApplication.java
@@ -1,16 +1,30 @@
 package jsug.flaky.members;
 
+import org.apache.catalina.filters.RequestDumperFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @SpringBootApplication
 public class FlakyMembersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlakyMembersApplication.class, args);
+	}
+
+	@Bean
+	@Profile("!cloud")
+	RequestDumperFilter dumperFilter() {
+		return new RequestDumperFilter();
 	}
 
 	@Configuration
@@ -21,4 +35,19 @@ public class FlakyMembersApplication {
 			config.exposeIdsFor(Member.class);
 		}
 	}
+
+	@Configuration
+	@EnableResourceServer
+	static class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+					.authorizeRequests().mvcMatchers("/members/search/findByGithub")
+					.permitAll().antMatchers(HttpMethod.POST, "/members").permitAll()
+					.antMatchers("/**").authenticated();
+		}
+
+	}
+
 }
